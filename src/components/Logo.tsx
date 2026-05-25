d85d73bd4dab9b335123f42b0eea9be7f9216421
Morishs <moyanscamara@gmail.com>
2026-05-25 04:06:48 +0000

    update

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { cn } from '../utils/cn';
+import { useTheme } from '../contexts/ThemeContext';
 
 type LogoProps = {
   logoClassName?: string;
@@ -14,10 +15,13 @@ export function Logo({
   to = '/',
   alt = 'Logo Myms',
 }: LogoProps) {
+  const { theme } = useTheme();
+  const logoSrc = theme === 'light' ? '/Logo_pdf.svg' : '/logo.svg';
+
   return (
     <Link to={to} className={cn('inline-flex items-center', containerClassName)}>
       <img
-        src="/logo.svg"
+        src={logoSrc}
         alt={alt}
         className={cn('h-10 w-auto', logoClassName)}
       />
